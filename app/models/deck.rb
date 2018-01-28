161083e02f00dc7800ad18cde28d21ea6aede8a5
Daniel Smith <dan@callrail.com>
2018-01-28 11:32:20 -0500

    Add deck_cards to Card and Deck models

--- app/models/deck.rb
+++ app/models/deck.rb
@@ -1,4 +1,27 @@
 class Deck < ApplicationRecord
   validates :user, presence: true
   belongs_to :user
+
+  has_many :deck_cards
+  has_many :cards, through: :deck_cards
+
+  validates :cards, length: { maximum: 100 }
+
+  before_save :deck_is_valid?
+  before_save :deck_is_standard_legal?
+  before_save :deck_is_expanded_legal?
+
+  def deck_is_valid?
+    self.valid_deck = cards.count == 60
+  end
+
+  def deck_is_standard_legal?
+    return unless cards.present?
+    self.standard_legal = cards.includes(:set).all? { |card| card.set.standard_legal? }
+  end
+
+  def deck_is_expanded_legal?
+    return unless cards.present?
+    self.expanded_legal = cards.includes(:set).all? { |card| card.set.expanded_legal? }
+  end
 end
