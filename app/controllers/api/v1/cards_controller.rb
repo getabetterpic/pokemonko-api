bf1782474530deb00cb0a91104dc8a99d975007e
Daniel Smith <dan@callrail.com>
2018-02-04 20:33:03 -0500

    Allow searching by set name and sort by number desc

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,8 +1,8 @@
 class Api::V1::CardsController < ApplicationController
   def search
-    cards = PkoCard.where('pko_cards.name ILIKE ?', params[:name] + '%')
-      .includes(:set)
-      .order("pko_sets.release_date DESC")
+    cards = PkoCard.includes(:set)
+      .where('pko_cards.name ILIKE ? OR pko_sets.name ILIKE ?', params[:name] + '%', params[:name] + '%')
+      .order("pko_sets.release_date DESC, regexp_replace(pko_cards.number, '[A-Za-z]+', '')::integer DESC")
     paginated = cards.paginate(page: params[:page].to_i || 1, per_page: 20)
     render json: { cards: paginated.as_json, total_cards: cards.count }, status: :ok
   end
